c361ddcb398bcf2220f784f218747c598f340fd9
schoolonlyaccount <macman5@tkf7.net>
2026-08-03 18:09:01 -0500

    Route and Query Parameters Activity Completed

--- src/models/projects.js
+++ src/models/projects.js
@@ -14,4 +14,18 @@ const getAllProjects = async () => {
     return result.rows;
 }
 
-export { getAllProjects }
+const getProjectsByOrganizationId = async (organizationId) => {
+    const query = `
+    SELECT project_id, organization_id, title, description, location, date
+    FROM public.service_project
+    WHERE organization_id = $1
+    ORDER BY date;
+    `;
+
+    const queryParams = [organizationId];
+    const result = await db.query(query, queryParams);
+
+    return result.rows;
+};
+
+export { getAllProjects, getProjectsByOrganizationId }
