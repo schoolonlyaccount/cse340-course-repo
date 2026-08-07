c92a131f257295ff8a581fa4e7d92d1090446606
schoolonlyaccount <macman5@tkf7.net>
2026-08-06 19:19:14 -0500

    Sessions and Flash Messages - Completed

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ import { fileURLToPath } from 'url';
 import path from 'path';
 import { testConnection } from './src/models/db.js';
 import router from './src/routes.js';
+import session from 'express-session';
+import flash from './src/middleware/flash.js';
+
+// ...
+const SESSION_SECRET = process.env.SESSION_SECRET;
 
 // Define the application environment
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
@@ -18,6 +23,17 @@ const app = express();
 /**
   * Configure Express middleware
   */
+
+// Set up session management
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60 * 60 * 1000 } // Session expires after 1 hour of inactivity
+}));
+
+// Use flash message middleware
+app.use(flash);
 
 // Allow Express to receive and process common POST data
 app.use(express.urlencoded({ extended: true }));
